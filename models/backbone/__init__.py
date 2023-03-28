b90ecc7007deab9cc53a1e1a5e7e166bad64706a
Linshan <52514670+Luffy03@users.noreply.github.com>
2023-03-28 14:08:34 +0800

    Add files via upload

---
+++ models/backbone/__init__.py
@@ -0,0 +1,8 @@
+from .resnet import ResNet18, ResNet34, ResNet50, ResNet101
+from .senet import se_resnext50_32x4d, se_resnext101_32x4d, se_resnet50, se_resnet101
+from .resnetstem import resnet50_v1c
+from .segformer import mit_b0, mit_b1, mit_b2, mit_b3, mit_b4, mit_b5
+from .swinformer import SwinTransformer
+
+from .convnext import ConvNeXt
+from .convnextv2 import convnextv2_base
